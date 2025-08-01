3d49f2e6067739bbbec84f9ff46e92438c2c2a64
EL-GOURCH97 <elgourch.jalal9@gmail.com>
2025-08-01 06:25:17 +0200

    Add files via upload

---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+echo "PHP fonctionne !";
+echo "<br>";
+echo "Date actuelle : " . date('Y-m-d H:i:s');
+?> 
---
+++ test_php.php
@@ -0,0 +1,7 @@
+<?php
+echo "PHP fonctionne !";
+echo "<br>";
+echo "Version PHP : " . phpversion();
+echo "<br>";
+echo "Date : " . date('Y-m-d H:i:s');
+?> 
